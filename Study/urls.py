51d1b9f3df795159c59d3f7daafe78dcd8cd8650
hjh200507609 <409762605@qq.com>
2019-12-10 18:05:49 +0800

    update

--- Study/urls.py
+++ Study/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from Study import views
 
 urlpatterns = [
-    url(r'^$', views.test1, name='index'),
-    url(r'^home$', views.test1, name='home'),
+    url(r'^gird_system$', views.girdSystem, name='index'),
+    url(r'^home$', views.login, name='home'),
     url(r'^login', views.login, name='login')
 ]
--- Study/views.py
+++ Study/views.py
@@ -1,13 +1,16 @@
 # coding=utf-8
 from django.shortcuts import render
-from django.shortcuts import render,get_object_or_404,get_list_or_404
-from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.cache import cache_page
 import json
+
+
 # Create your views here.
 
-def test1(request):
-    pass
+def girdSystem(request):
+    return render(request, template_name='Study/gird_system.html')
+
 
 def login(request):
-    return render(request,template_name='Study/test_login.html')
+    return render(request, template_name='Study/test_login.html')
