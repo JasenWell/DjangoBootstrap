41e9dbbe7a0deea09bd5e946134bc29492b45737
hjh200507609 <409762605@qq.com>
2019-12-11 17:13:03 +0800

    Update views.py

--- Entry/views.py
+++ Entry/views.py
@@ -2,13 +2,25 @@
 from django.shortcuts import render,get_object_or_404,get_list_or_404
 from django.http import HttpResponseRedirect,HttpResponse
 from django.views.decorators.cache import cache_page
-import json
+from django.template.loader import render_to_string
+import json,os
 
 
 # Create your views here.
 
+def generateStaticPage(request):
+    staticHtml = 'test.html'
+    context = {'key':'value'}
+    if not os.path.exists(staticHtml):
+        content = render_to_string('base.html',context)
+        with open(staticHtml,'w')as f:
+            f.write(content)
+    return render(request,template_name=staticHtml)
+
 def index(request):
     return HttpResponse('中文hello')
+
+
 
 
 def home(request):
